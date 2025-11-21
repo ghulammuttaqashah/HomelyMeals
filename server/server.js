36980b6b5bcbbb6643cd5063386f95db556524f4
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-11-21 21:55:39 +0500

    added cors in sevrer.js

--- server/server.js
+++ server/server.js
@@ -6,30 +6,33 @@ import cors from "cors";
 import connectDB from "./shared/config/db.js";
 import { PORT } from "./shared/config/env.js";
 import cookieParser from "cookie-parser";
-import adminRoutes from "./modules/admin/index.js";  
 
+import adminRoutes from "./modules/admin/index.js";
 import customerRoutes from "./modules/customer/index.js";
 import cookRoutes from "./modules/cook/index.js";
 
 const app = express();
 
-app.use(cookieParser());
+// Correct CORS for cookies
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend
+    credentials: true, // allow cookies to be sent/received
+  })
+);
 
-// Middleware
-app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 // Database
 connectDB();
+
 app.get("/", (req, res) => {
   res.send("Homely Meals API is working!");
 });
 
-
-
 app.use("/api/admin", adminRoutes);
-// Routes
-app.use("/api/customers", customerRoutes);
+app.use("/api/customer", customerRoutes);
 app.use("/api/cooks", cookRoutes);
 
 app.listen(PORT, () => {
